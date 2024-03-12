0b9a88cb27fd376cfede19f0b957556c50ac6e78
Iwn Elion <153987262+iwnelion@users.noreply.github.com>
2024-03-12 13:27:17 +0200

    Create global variable.py

---
+++ global variable.py
@@ -0,0 +1,11 @@
+Python 3.11.2 (main, Mar 13 2023, 12:18:29) [GCC 12.2.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+def func():
+    global x
+    x="Jo"
+
+    
+func()
+
+print("hello "+x)
+hello Jo
