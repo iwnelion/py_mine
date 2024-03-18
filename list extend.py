aad852914899920264f72072af9c9b18eb50aac3
Iwn Elion <153987262+iwnelion@users.noreply.github.com>
2024-03-18 11:26:10 +0200

    Create list extend.py

---
+++ list extend.py
@@ -0,0 +1,6 @@
+myList=["apple","banana","cherry"]
+tropical=["mango","pinapple"]
+myList.extend(tropical)
+print(myList)
+#['apple', 'banana', 'cherry', 'mango', 'pinapple']
+#adds the second list to the first list
