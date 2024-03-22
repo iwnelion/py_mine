d54068c03debadfd6f308e81ce988db4f4639b62
Iwn Elion <153987262+iwnelion@users.noreply.github.com>
2024-03-22 11:18:18 +0200

    Create funct call.py

---
+++ funct call.py
@@ -0,0 +1,13 @@
+def compare(a,b):
+    if a>b:
+        print("a")
+    elif a<b:
+        print("b")
+    else:
+        print("=")
+
+compare(7,9)    #b
+x=12
+y=5
+compare(x,y)    #a
+compare(x,12)   #=
